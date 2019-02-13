65ba5e0fecb588f5f65bac231937792309f12d43
Jongmin Kim <jongmin.kim@oakton.com.au>
2019-02-14 09:19:31 +1100

    react app

---
+++ precache-manifest.1254189359e75959a96f6e2f88f2525c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "19836b33ce2a15968bc8",
+    "url": "/static/js/main.19836b33.chunk.js"
+  },
+  {
+    "revision": "092ce962f6abde68047e",
+    "url": "/static/js/2.092ce962.chunk.js"
+  },
+  {
+    "revision": "19836b33ce2a15968bc8",
+    "url": "/static/css/main.8ed4edb0.chunk.css"
+  },
+  {
+    "revision": "c6d9b98adb73f9716073fbfb8a945e6e",
+    "url": "/index.html"
+  }
+];
